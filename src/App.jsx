ede63c8978920be48cd86e0eadb397462abf08f1
Fernando Alves <lfalvespe@gmail.com>
2023-07-29 18:32:01 -0300

    update 06_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './App.css'
+
 // import HookState_01 from "./components/UseState_01";
 // import HookState_02 from "./components/UseState_02";
 // import HookState_03 from "./components/UseState_03";
@@ -10,7 +12,9 @@ import HookState_06 from "./components/UseState_06";
 export default function () {
     return(
         <>
-            <h1>React Hooks - useState</h1>
+            <header>
+                <h1>React Hooks - useState</h1>
+            </header>
             <hr />
             <HookState_06 />
         </>
@@ -18,7 +22,3 @@ export default function () {
 }
 
 
-// Um componente pai vai receber atualização de valores de dois componentes filhos.
-// Esses componentes filhos são instâncias do mesmo componente contador (inc e dec)
-// Um dos componentes incrementa e decrementa de 5 em 5 e o outro de 10 em 10. 
-// No componente pai, vai ser apresentado o resultado da adição dos valores controlados pelos dois componentes filhos.
--- src/components/UseState_06.jsx
+++ src/components/UseState_06.jsx
@@ -9,33 +9,45 @@ export default function () {
     const [valor1, setValor1] = useState(0)
     const [valor2, setValor2] = useState(0)
 
-    function dec1()
-    {
-        setValor1(v1=> v1-5)
+    function dec1() {
+        setValor1(v1 => v1 - 5)
     }
-    function inc1()
-    {
-        setValor1(v1=> v1+5)
+    function inc1() {
+        setValor1(v1 => v1 + 5)
     }
 
-    function dec2()
-    {
-        setValor2(v2=> v2-10)
+    function dec2() {
+        setValor2(v2 => v2 - 10)
     }
-    function inc2()
-    {
-        setValor2(v2=> v2+10)
+    function inc2() {
+        setValor2(v2 => v2 + 10)
     }
 
-    return(
+    return (
         <>
-            <p>Valor1: {valor1}</p>
-            <p>Valor2: {valor2}</p>
-            <UseState_06a inc={inc1} dec={dec1}/>
+            <p>Valor1: <strong>{valor1}</strong></p>
+            <p>Valor2: <strong>{valor2}</strong></p>
+            <UseState_06a inc={inc1} dec={dec1} />
             <hr />
-            <UseState_06a inc={inc2} dec={dec2}/>
+            <UseState_06a inc={inc2} dec={dec2} />
             <hr />
-            <p>A soma de {valor1} com {valor2} vale: {valor1 + valor2}</p>
+            <h3>A soma de {valor1} com {valor2} vale: {valor1 + valor2}</h3>
+            <hr />
+
+            <div className="exerc">
+                <hr />
+                <h3>Exercício:</h3>
+                <p> - Um componente pai vai receber atualização de valores de dois componentes filhos.</p>
+                <p>- Esses componentes filhos são instâncias do mesmo componente contador (inc e dec)</p>
+                <p>- Um dos componentes incrementa e decrementa de 5 em 5 e o outro de 10 em 10. </p>
+                <p>
+                    - No componente pai, vai ser apresentado o resultado da adição dos valores controlados pelos dois componentes filhos.
+                </p>
+                <hr />
+            </div>
+
+
         </>
     )
+
 }
